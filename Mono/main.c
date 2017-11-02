3412bbc9e6dcc288dbe6e382cf24e71fa3c3067c
dymbasss <32347004+dymbasss@users.noreply.github.com>
2017-11-02 12:07:21 +0300

    Update main.c

--- Mono/main.c
+++ Mono/main.c
@@ -22,8 +22,8 @@ int fib_n(int n)
 int main()
 {
 	FILE *fp; 
-	char *name = "/home/fox/Mono/fib.txt";
-	char *name2 = "/home/fox/Mono/mono.txt";
+	char *name = "/home/fox/DSR/Mono/fib.txt";
+	char *name2 = "/home/fox/DSR/Mono/mono.txt";
 	int n = 0, y = 0;
 	char c;
 
